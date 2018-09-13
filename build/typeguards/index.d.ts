3016fedbd39fb4c89dcc6638235d66186b054bdf
dominic.griesel <d.griesel@gmx.net>
2018-09-13 13:45:46 +0200

    Implement isObject and isArray typeguards

---
+++ build/typeguards/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Tests whether the given variable is a real object and not an Array
+ * @param it The variable to test
+ */
+export declare function isObject(it: unknown): boolean;
+/**
+ * Tests whether the given variable is really an Array
+ * @param it The variable to test
+ */
+export declare function isArray(it: unknown): boolean;
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as deferredPromise from "./deferred-promise";
 import * as math from "./math";
 import * as objects from "./objects";
 import * as strings from "./strings";
+import * as typeguards from "./typeguards";
 import * as types from "./types";
 
 export {
@@ -15,5 +16,6 @@ export {
 	math,
 	objects,
 	strings,
+	typeguards,
 	types,
 };
