362a7b33b0cd491f2027754b556690b7d36c5964
Dominic Griesel <d.griesel@gmx.net>
2021-05-04 14:55:46 +0200

    chore: update typedoc

--- typedoc.js
+++ typedoc.js
@@ -1,15 +1,11 @@
 module.exports = {
 	out: "docs/",
-	mode: "modules",
-	moduleResolution: "node",
 	exclude: [
 		"**/src/index.ts",
 		"**/src/**/*.test.ts",
 		"**/src/types/*.ts"
 	],
 	externalPattern: "node_modules",
-	excludeNotExported: true,
 	excludePrivate: true,
-	target: "ES2015",
 	readme: "none",
 };
