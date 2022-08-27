10747b8e3149c7826e3c69f4713aec5fc1d10180
Dominic Griesel <d.griesel@gmx.net>
2022-08-27 14:38:41 +0200

    fix: wasn't enough :(

--- build/typeguards/index.d.ts
+++ build/typeguards/index.d.ts
@@ -3,8 +3,12 @@
  * @param it The variable to test
  */
 export declare function isObject<T>(it: T): it is T & Record<string, unknown>;
+declare type IfAny<T, Y, N> = 0 extends (1 & T) ? Y : N;
+declare type IsAny<T> = IfAny<T, true, never>;
+declare type ExtractArray<T> = true extends IsAny<T> ? unknown[] : T extends unknown[] ? T : unknown extends T ? (T & unknown[]) : never;
 /**
  * Tests whether the given variable is really an Array
  * @param it The variable to test
  */
-export declare function isArray<T>(it: T): it is T & unknown[];
+export declare function isArray<T>(it: T): it is T & ExtractArray<T>;
+export {};
--- src/typeguards/index.ts
+++ src/typeguards/index.ts
@@ -10,13 +10,20 @@ export function isObject<T>(it: T): it is T & Record<string, unknown> {
 	return Object.prototype.toString.call(it) === "[object Object]";
 }
 
-type ExtractArray<T> = T extends unknown[] ? T : (T & unknown[]);
+type IfAny<T, Y, N> = 0 extends (1 & T) ? Y : N; 
+type IsAny<T> = IfAny<T, true, never>;
+
+type ExtractArray<T> = 
+     true extends IsAny<T> ? unknown[]
+     : T extends unknown[] ? T
+     : unknown extends T ? (T & unknown[])
+     : never;
 
 /**
  * Tests whether the given variable is really an Array
  * @param it The variable to test
  */
-export function isArray<T>(it: T): it is ExtractArray<T> {
+export function isArray<T>(it: T): it is T & ExtractArray<T> {
 	if (Array.isArray != null) return Array.isArray(it);
 	return Object.prototype.toString.call(it) === "[object Array]";
 }
