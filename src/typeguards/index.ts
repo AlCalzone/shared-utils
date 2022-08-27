79534499fb09d4f0a83370b7038de1fb27fe2c5f
Dominic Griesel <d.griesel@gmx.net>
2022-08-27 14:24:05 +0200

    fix: update isArray to support TS 4.8 changes

--- src/typeguards/index.ts
+++ src/typeguards/index.ts
@@ -10,11 +10,13 @@ export function isObject<T>(it: T): it is T & Record<string, unknown> {
 	return Object.prototype.toString.call(it) === "[object Object]";
 }
 
+type ExtractArray<T> = T extends unknown[] ? T : (T & unknown[]);
+
 /**
  * Tests whether the given variable is really an Array
  * @param it The variable to test
  */
-export function isArray<T>(it: T): it is T & unknown[] {
+export function isArray<T>(it: T): it is ExtractArray<T> {
 	if (Array.isArray != null) return Array.isArray(it);
 	return Object.prototype.toString.call(it) === "[object Array]";
 }
